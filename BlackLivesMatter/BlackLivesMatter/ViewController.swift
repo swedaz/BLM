89394d95a6e68037b53bee5829caf99aaa8f8679
Mohandas, Swetha <smohandas3@gatech.edu>
2020-06-10 12:32:34 -0700

    commit message goes here

---
+++ BlackLivesMatter/BlackLivesMatter/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BlackLivesMatter
+//
+//  Created by Swetha Mohandas on 6/10/20.
+//  Copyright © 2020 Sambar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+}
+
